f8e18e6654f2f96e79fc1cb1f8d08bfa261efb25
Shounak Lohokare <shounaklohokare@gmail.com>
2022-11-13 14:45:32 +0530

    refactored the code

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ function updateInput(e){
 }
 
 
-async function curatedPhotos(){
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1",
-    {
+async function fetchApi(url){
+    const dataFetch = await fetch(url, {
         method : "GET",
         headers : {
             Accept : "application/json",
@@ -26,6 +25,10 @@ async function curatedPhotos(){
         }
     });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data){
     data.photos.forEach(photo => {
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
@@ -33,28 +36,18 @@ async function curatedPhotos(){
         <p>${photo.photographer}</p>`;
         gallery.appendChild(galleryImg);
     })
+    
+}
 
+async function curatedPhotos(){ 
+    const data = await fetchApi('https://api.pexels.com/v1/curated?per_page=15&page=1');
+    generatePictures(data);
 }
 
 async function searchPhotos(query){
-    const dataFetch = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`,
-    {
-        method : "GET",
-        headers : {
-            Accept : "application/json",
-            Authorization : auth
-        }
-    });
-    const data = await dataFetch.json();
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`)
     console.log(data);
-    data.photos.forEach(photo => {
-        const galleryImg = document.createElement('div');
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>`;
-        gallery.appendChild(galleryImg);
-    })
-
+   generatePictures(data);
 }
 
 curatedPhotos();
